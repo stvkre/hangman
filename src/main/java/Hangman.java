538c355a6e4f98726d4730236ad6029a4c39067f
stvkre <kiariemburuh@gmail.com>
2017-04-22 23:19:40 +0300

    While loop
    Introduced a boolean data type to show the game is in progress.

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,12 +1,17 @@
 import java.io.Console;
+// import java.util.Random;
 
 public class Hangman {
   public static void main(String[] args) {
+    Random random = new Random();
     String[] guesses = { "constipate", "oblitirate", "athletism", "propaganda", "sequel", "quantum" };
 
     Console myConsole = System.console();
 
 // Introducing player
+
+    boolean GameInProgress = true;
+    while (GameInProgress) {
 
     System.out.println("Enter your Name:");
     String hangMan = myConsole.readLine();
@@ -15,6 +20,11 @@ public class Hangman {
     System.out.println("Here is how our gane works, You can only guess one letter at a time. if you guess whether right or wrong the computer automatically moves to the next letter up until the spaces are used up. How you win is by correctly guessing the words according to the spaces given. Good Luck");
     System.out.println("---------------------");
 
-      for ( )
+    char[] anyWordGuessed = guesses[random.nextInt(guesses.length)].toCharArray();
+    int numberOf Guesses =
+    // saving user input
+    char[] playerGuess = new char[numberOfGuesses];
+}
+    System.out.println("Game Over");
   }
 }
