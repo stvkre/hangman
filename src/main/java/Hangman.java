c855b03ec0c2940234cae5533b6f8e6f1bfcfd8a
stvkre <kiariemburuh@gmail.com>
2017-04-22 23:56:26 +0300

    New Game
    Added a segment for a new game. That is if the player wants to continue playing

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,5 +1,6 @@
 import java.io.Console;
-// import java.util.Random;
+import java.util.Scanner;
+import java.util.Random;
 
 public class Hangman {
   public static void main(String[] args) {
@@ -20,11 +21,37 @@ public class Hangman {
     System.out.println("Here is how our gane works, You can only guess one letter at a time. if you guess whether right or wrong the computer automatically moves to the next letter up until the spaces are used up. How you win is by correctly guessing the words according to the spaces given. Good Luck");
     System.out.println("---------------------");
 
-    char[] anyWordGuessed = guesses[random.nextInt(guesses.length)].toCharArray();
-    int numberOf Guesses =
     // saving user input
-    char[] playerGuess = new char[numberOfGuesses];
+    char[] anyWordGuessed = guesses[random.nextInt(guesses.length)].toCharArray();
+    int numberOfGuesses = anyWordGuessed.length;
+
+    char[] PlayerGuess = new char[numberOfGuesses];
+
+// replacing word to be guessed by the player with an underscore sign
+    for (int i = 0; i < PlayerGuess.length; i++) {
+      PlayerGuess[i] = '_';
+    }
+
+    boolean wordIsGuessed = false;
+    int tries = 0;
+
+  while (!wordIsGuessed && tries != numberOfGuesses) {
+    System.out.println("Current guesses: ");
+    printArray(PlayerGuess);
+    System.out.printf("You have %d tries left.\n", numberOfGuesses - tries);
+    System.out.println("Enter a single letter");
+
+  }
+
 }
     System.out.println("Game Over");
+  }
+
+// New game after the player chooses to play again
+
+  public static void printArray(char[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
+    }
   }
 }
